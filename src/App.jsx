d3a091c27b68283d1d88eb21e73bfbac7eda287b
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-09-25 13:54:58 +0700

    fix: forgor to add the `Preview` page to the router

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AllPosts from './pages/AllPosts'
 import './App.css'
 import AddNew from './pages/AddNew'
 import EditPost from './pages/EditPost'
+import Preview from './pages/Preview'
 
 const App = () => {
   return (
@@ -14,11 +15,13 @@ const App = () => {
           <nav className='flex gap-4 mb-6'>
             <Link to="/">All Posts</Link>
             <Link to="/add">Add New</Link>
+            <Link to="/preview">Preview</Link>
           </nav>
           <Routes>
             <Route path="/" element={<AllPosts />} />
             <Route path="/add" element={<AddNew />} />
             <Route path="/edit/:id" element={<EditPost />} />
+            <Route path="/preview" element={<Preview />} />
           </Routes>
         </div>
       </Router>
